94939fb44a0d9a48a88c790af67b9df8f9d76e16
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-01-04 14:15:54 +0100

    Changing directory naming

--- nvim/lua/custom_lua/init.lua
+++
@@ -1,4 +0,0 @@
-require("custom_lua.remap")
-require("custom_lua.packer")
-require("custom_lua.set")
-require("custom_lua.filetype")
---
+++ nvim/lua/customize/init.lua
@@ -0,0 +1,4 @@
+require("customize.remap")
+require("customize.lazy_nvim")
+require("customize.set")
+require("customize.filetype")
