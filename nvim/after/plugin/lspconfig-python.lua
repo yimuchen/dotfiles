5fc4dd30f1c4bec265cd25e9cd3c5c79226df5c9
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-03-26 14:47:46 +0100

    Added the custom formatting and snippet engine

--- nvim/after/plugin/lspconfig-python.lua
+++ nvim/after/plugin/lspconfig-python.lua
@@ -1,18 +1,38 @@
 -- Mason will not be used for the various installation, as black, flake8 and
 -- isort is are plugins not registered under mason.
-require("lspconfig").pylsp.setup({
-	settings = {
-		pylsp = {
-			plugins = {
-				-- Use flake8 instead of pycodestyle
-				flake8 = { enabled = true },
-				pycodestyle = { enabled = false },
-				-- Default formatting
-				black = { enabled = true },
-				isort = { enable = true },
-			},
-		},
-	},
-})
+require('lspconfig').pylsp.setup {
+  settings = {
+    pylsp = {
+      plugins = {
+        -- Use flake8 instead of pycodestyle
+        flake8 = { enabled = true },
+        pycodestyle = { enabled = false },
+        -- Default formatting
+        black = { enabled = true },
+        isort = { enable = true },
+      },
+    },
+  },
+}
 
-require("conform").formatters_by_ft.python = { "isort", "black" }
+-- Formatting methods
+require('conform').formatters_by_ft.python = { 'isort', 'black' }
+
+-- Snippets
+local ls = require 'luasnip'
+--local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+--local f = ls.function_node
+--local c = ls.choice_node
+--local d = ls.dynamic_node
+--local r = ls.restore_node
+
+ls.add_snippets('python', {
+  -- trigger is `fn`, second argument to snippet-constructor are the nodes to
+  -- insert into the buffer on expansion.
+  ls.snippet('__main__', {
+    t { 'if __name__ == "__main__":', '    ' },
+    i(0),
+  }),
+})
--- nvim/lua/customize/lazy_nvim.lua
+++ nvim/lua/customize/lazy_nvim.lua
@@ -1,74 +1,88 @@
 -- Boot strapwith without lazy.nvim being explciitly installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Listing packages to install
-require("lazy").setup({
-	-- Having lazy manage itself
-	"folke/lazy.nvim",
+require('lazy').setup {
+  -- Having lazy manage itself
+  'folke/lazy.nvim',
 
-	-- Fuzzy finder telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"nvim-telescope/telescope-fzf-native.nvim",
-		},
-	},
+  -- Fuzzy finder telescope
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-telescope/telescope-fzf-native.nvim',
+    },
+  },
 
-	-- Monokai color scheme
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			require("monokai-pro").setup()
-		end,
-	},
-	-- Lua line settings
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+  -- Monokai color scheme
+  {
+    'loctvl842/monokai-pro.nvim',
+    config = function()
+      require('monokai-pro').setup()
+    end,
+  },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
 
-	-- Treesitter
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "mbbill/undotree" },
-	{ "tpope/vim-fugitive" },
+  -- Lua line settings
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
 
-	-- LSP zero and language related plugins
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-	{ "neovim/nvim-lspconfig" },
-	{ "nvimtools/none-ls.nvim" }, -- For additional formatting methods
-	{ "williamboman/mason.nvim" }, -- Engine for managing external dependencies
-	{ "williamboman/mason-lspconfig.nvim" },
-	{ "WhoIsSethDaniel/mason-tool-installer.nvim" }, -- Formatter and linting tool installation
-	{ "hrsh7th/nvim-cmp" }, -- For auto completion
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "stevearc/conform.nvim" }, -- Formatting engine
+  -- Treesitter
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'mbbill/undotree' },
+  { 'tpope/vim-fugitive' },
 
-	-- Snippet engine
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		build = "make in stall_jsregexp",
-	},
-	{ "saadparwaiz1/cmp_luasnip" },
+  -- LSP zero and language related plugins
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+  { 'neovim/nvim-lspconfig' },
+  { 'nvimtools/none-ls.nvim' }, -- For additional formatting methods
+  { 'williamboman/mason.nvim' }, -- Engine for managing external dependencies
+  { 'williamboman/mason-lspconfig.nvim' },
+  { 'WhoIsSethDaniel/mason-tool-installer.nvim' }, -- Formatter and linting tool installation
+  -- For auto completion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      { -- Snippet engine for writing custom snippeds
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        build = 'make in stall_jsregexp',
+      },
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+    },
+  },
+  -- Formatting engine
+  { 'stevearc/conform.nvim' },
 
-	-- For VIM motion training
-	{ "ThePrimeagen/vim-be-good" },
+  -- For VIM motion training
+  { 'ThePrimeagen/vim-be-good' },
 
-	-- Notebook editing in VIM
-	{ "GCBallesteros/jupytext.nvim", config = true },
-})
+  -- Notebook editing in VIM
+  { 'GCBallesteros/jupytext.nvim', config = true },
+}
+  -- Snippet engine
