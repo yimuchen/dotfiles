139874050e8ae7c74ebd3779f568d09c9d15936c
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-04-04 14:06:25 +0200

    Setting relative path in status line

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,13 @@
-require("lualine").setup({
-	options = {
-		theme = "onedark",
-	},
-})
+require('lualine').setup {
+  options = {
+    theme = 'onedark',
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+}
