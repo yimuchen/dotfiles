84fa8e06815299749305eaef93f5daec4b565fde
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-04-16 11:25:56 +0200

    Fixed default settings for sh formatting

--- nvim/after/plugin/lspconfig-sh.lua
+++ nvim/after/plugin/lspconfig-sh.lua
@@ -11,4 +11,10 @@ require('mason-tool-installer').setup {
 }
 
 -- Additional methods for formatting (Use spaces rather than tabs)
-require('conform').formatters_by_ft.sh = { 'shfmt' }
+local conform = require 'conform'
+conform.formatters.shfmt = {
+  prepend_args = function()
+    return { '-i', '3' }
+  end,
+}
+conform.formatters_by_ft.sh = { 'shfmt' }
