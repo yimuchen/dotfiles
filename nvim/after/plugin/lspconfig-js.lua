19dbb99a84558a85379d4a5b31e67d87eba489f7
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-04-30 15:37:36 +0200

    Naming the formatter to something separate

--- nvim/after/plugin/lspconfig-js.lua
+++ nvim/after/plugin/lspconfig-js.lua
@@ -6,11 +6,11 @@ require('lspconfig').cssls.setup {}
 local conform = require 'conform'
 
 -- Setting the prettier to detect the pretteir install the node environment
-conform.formatters.prettier = {
+conform.formatters.npx_prettier = {
   command = 'npx',
   args = { 'prettier', '--stdin-filepath', '$FILENAME' },
 }
-conform.formatters_by_ft.svelte = { 'prettier' }
-conform.formatters_by_ft.css = { 'prettier' }
-conform.formatters_by_ft.javascript = { 'prettier' }
-conform.formatters_by_ft.html = { 'prettier' }
+conform.formatters_by_ft.svelte = { 'npx_prettier' }
+conform.formatters_by_ft.css = { 'npx_prettier' }
+conform.formatters_by_ft.javascript = { 'npx_prettier' }
+conform.formatters_by_ft.html = { 'npx_prettier' }
