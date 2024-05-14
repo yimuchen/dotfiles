9a0ce827471c1a99e627d8acb021973174c55529
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-05-14 10:36:32 +0200

    Fixed formatting issue with svelte files

--- nvim/after/plugin/lspconfig-js.lua
+++ nvim/after/plugin/lspconfig-js.lua
@@ -10,7 +10,19 @@ conform.formatters.npx_prettier = {
   command = 'npx',
   args = { 'prettier', '--stdin-filepath', '$FILENAME' },
 }
-conform.formatters_by_ft.svelte = { 'npx_prettier' }
+conform.formatters_by_ft.html = { 'npx_prettier' }
 conform.formatters_by_ft.css = { 'npx_prettier' }
 conform.formatters_by_ft.javascript = { 'npx_prettier' }
-conform.formatters_by_ft.html = { 'npx_prettier' }
+
+-- Special case for svelte, as neovim detects this as an embeded language and
+-- will try and inject ';' characters in in-line svelte-js code
+conform.formatters_by_ft.svelte = { 'npx_prettier' }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'svelte' },
+  callback = function()
+    vim.keymap.set('n', '<leader>fb', function()
+      conform.format { async = true, formatters = { 'npx_prettier', 'trim_whitespace' } }
+    end, { desc = '[F]ormat selected [B]uffer' })
+  end,
+})
--- nvim/after/plugin/lspconfig-md.lua
+++ nvim/after/plugin/lspconfig-md.lua
@@ -6,10 +6,9 @@ vim.api.nvim_create_autocmd('FileType', {
     local node_path = vim.fn.getcwd() .. '/node_modules'
     if vim.fn.isdirectory(node_path) == 1 then
       -- Use prettier for markdown formatter for blog posts/site generators
-      require('conform').formatters_by_ft.markdown = { 'prettier' }
+      require('conform').formatters_by_ft.markdown = { 'npx_prettier' }
     else
       require('conform').formatters_by_ft.markdown = { 'mdformat' }
     end
   end,
 })
-
