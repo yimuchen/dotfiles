addbdfd750e6bdc608d5596026efa94117c0ce82
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-06-07 17:00:51 +0200

    First commit for tmux configurations

--- nix-config/modules/misc.nix
+++ nix-config/modules/misc.nix
@@ -1,9 +1,9 @@
 # Miscellaneous packages with no additional configurations needed.
 { pkgs, config, ... }: {
-  home.packages = [
-    pkgs.libsixel
-    pkgs.micromamba
-    pkgs.root
-  ];
+  home.packages = [ pkgs.libsixel pkgs.micromamba pkgs.root pkgs.tmux ];
+
+  # Link to the major configuration path
+  home.file.".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.config/home-manager/tmux.conf";
 }
 
