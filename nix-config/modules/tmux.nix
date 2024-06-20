3f244b7d58041eaf72ead801210d16807a5da61e
Yi-Mu Chen <enochnotsocool@gmail.com>
2024-06-20 11:11:49 +0200

    Changing to using nix for tmux plugin management

--- nix-config/modules/tmux.nix
+++ nix-config/modules/tmux.nix
@@ -1,11 +1,26 @@
 # Configuration for tmux for multiplexing and sessiong saving
 
 { pkgs, config, ... }: {
-  home.packages = [
-    pkgs.tmux # For multiplexing and session saving
-  ];
-  # Link to the major configuration path
-  home.file.".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/tmux.conf";
+  programs.tmux = {
+    enable = true;
+    newSession = true; # Spawn new session in nothing is running
+    terminal = "xterm-256color";
+    plugins = [ # Plugins listed in the official nix repository
+      {
+        plugin = pkgs.tmuxPlugins.rose-pine; # For theming
+        extraConfig = ''
+          set -g @rose_pine_variant 'main'
+          set -g @rose_pine_host 'on' # Enables hostname in the status bar
+          set -g @rose_pine_user 'on' # Turn on the username component in the statusbar
+          set -g @rose_pine_directory 'on'
+        '';
+      }
+    ];
+    extraConfig = ''
+      set -g allow-passthrough on # Allowing passthrough
+      bind-key -T  prefix N new-window "zsh -c 'nvim .'"
+      bind-key -T  prefix M new-window "htop --user $USER"
+    '';
+  };
 }
 
