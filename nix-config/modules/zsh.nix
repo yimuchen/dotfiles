e48441f87ebafb08b2d79fbb6a7ac672c1682e78
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-01 15:26:21 +0200

    First attempt to move zsh configuration to nix

--- nix-config/modules/zsh.nix
+++ nix-config/modules/zsh.nix
@@ -1,17 +1,27 @@
-# NIX Configurations for zsh. Due to the complexity of the configuration, the
-# main configuration will still use the nix-less methods. Nix is just
-# responsible for setting up the symbolic link in the home directory.
 { config, ... }: {
   # Additional packages that is required to use the various configuration.
   # Because zsh should be considered a system package, do *not* put zsh here.
 
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    antidote = {
+      enable = true;
+      plugins = [
+        "romkatv/powerlevel10k" # For the theming
+        "conda-incubator/conda-zsh-completion.git" # For conda autocomplete
+      ];
+    };
+  };
   # Package management is mainly handled by a automatic pull of oh-my-zsh
 
   home.file = {
     # My configuration files
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/home-manager/zsh/zshrc.zsh";
-    ".zsh".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/home-manager/zsh";
+    #".zshrc".source = config.lib.file.mkOutOfStoreSymlink
+    #  "${config.home.homeDirectory}/.config/home-manager/zsh/zshrc.zsh";
+    #".zsh".source = config.lib.file.mkOutOfStoreSymlink
+    #  "${config.home.homeDirectory}/.config/home-manager/zsh";
   };
 }
