baadaba035a324bef6c8d72521eb1675adfc2420
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-02 11:04:09 +0200

    Adding autocomplete for helper shell scripts

--- nix-config/modules/shell-helper.nix
+++ nix-config/modules/shell-helper.nix
@@ -1,36 +1,34 @@
-# Configurations for adding python developement environments and system helper
-# scripts
-#
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+let
+  pdftopng = pkgs.writeScriptBin "pdftopng.py"
+    (builtins.readFile ../../pyscripts/pdftopng.py);
+  keepassxc_cli = pkgs.writeScriptBin "keepassxc_cli.py"
+    (builtins.readFile ../../pyscripts/keepassxc_cli.py);
+  nix_check_update = pkgs.writeScriptBin "nix-check-update.py"
+    (builtins.readFile ../../pyscripts/nix-check-update.py);
+in {
+  # Configurations for adding system helper scripts
   home.packages = [
     # Our main packages all require python
     (pkgs.python3.withPackages (ps: [
-      # Additional items required to run the various helper scripts For
-      # autocompletion
-      ps.argcomplete
-      # For extracting data in keepassxc database
-      ps.pykeepass
-      # Smaller packages probably need these setup modes
-      ps.setuptools
-      # For progress bars
-      ps.tqdm
-      # For imagemagik python bindings
-      ps.wand
+      ps.argcomplete # Autocompletion of scripts
+      ps.pykeepass # Extracting data in keepassxc database
+      ps.setuptools # Nonstandard packages needs setup modules
+      ps.tqdm # For progress bars
+      ps.wand # For imagemagick python bindings
     ]))
 
-    # For PDF conversion
-    pkgs.imagemagick
-
-    # PDF conversion script
-    (pkgs.writeScriptBin "pdftopng.py"
-      (builtins.readFile ../../pyscripts/pdftopng.py))
-    # Interactions with PDF
-    (pkgs.writeScriptBin "keepassxc_cli.py"
-      (builtins.readFile ../../pyscripts/keepassxc_cli.py))
-    # Method for calling the obtaining package version informations
-    (pkgs.writeScriptBin "nix-check-update.py"
-      (builtins.readFile ../../pyscripts/nix-check-update.py))
+    pdftopng # PDF to PNG batch conversion script
+    keepassxc_cli # Interacting with keepassxc for CLI credential interactions
+    nix_check_update # Checking for nix updates upstream
   ];
+  # Additional set-up to allow for autocompletion
+  programs.zsh.envExtra = ''
+    activate-global-python-argcomplete
+    eval "$(register-python-argcomplete ${pdftopng}/bin/pdftopng.py)"
+    eval "$(register-python-argcomplete ${keepassxc_cli}/bin/keepassxc_cli.py)"
+    eval "$(register-python-argcomplete ${nix_check_update}/bin/nix-check-update.py)"
+  '';
 
   # Additional helper to keep track of home-manager packages
   home.file.".local/state/hm-packages".text = let
