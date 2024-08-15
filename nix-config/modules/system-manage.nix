1031993a86e15bdc46fd580d0047382a07e01e54
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-15 18:19:48 +0200

    Addition aliases for performing system management

---
+++ nix-config/modules/system-manage.nix
@@ -0,0 +1,10 @@
+{ pkgs, config, ... }: {
+  # Additional helper methods for system helping
+  programs.zsh.shellAliases = {
+    # Power related aliases
+    "poweroff" = "systemctl poweroff";
+    "reboot" = "systemctl reboot";
+    "efireboot" = "systemctl reboot --firmware-setup";
+  };
+
+}
