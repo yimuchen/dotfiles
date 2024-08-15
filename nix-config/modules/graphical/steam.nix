8af98023adde2248dd6e42f7c054b026e6e5bfcf
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-15 11:09:06 +0200

    Bumping version

--- nix-config/modules/graphical/steam.nix
+++ nix-config/modules/graphical/steam.nix
@@ -5,6 +5,7 @@
     # pkgs.stream # Steam requires a global setup as it requires networking
     pkgs.r2modman # For dyson sphere program!
     pkgs.lutris # For other launchers
+    # This is an update test
   ];
 
   # Find a way to automatically install and update proton-ge?
