d4c4000be47698d52c70a12285a7282c54c16039
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-28 14:45:23 +0200

    Removing redundent comments

--- nvim/lua/customize/lazy/treesitter.lua
+++ nvim/lua/customize/lazy/treesitter.lua
@@ -4,28 +4,16 @@ return {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup {
-        -- A list of parser names, or "all" (the five listed parsers should always be
-        -- installed)
-        ensure_installed = { 'c', 'cpp', 'python', 'lua', 'query' },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
+        ensure_installed = { 'c', 'cpp', 'python', 'lua', 'query', 'nix' },
         sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer Recommendation:
-        -- set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = true,
 
         highlight = {
           enable = true,
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same
-          -- time. Set to `true` if you depend on 'syntax' being enabled (like for
-          -- indentation). Using this option may slow down your editor, and you may
-          -- see some duplicate highlights. Instead of true it can also be a list of
-          -- languages
           additional_vim_regex_highlighting = false,
         },
 
-        -- Symbol based navigations
+        -- Enabling additional symbol-based navigation
         textobjects = {
           move = {
             enable = true,
