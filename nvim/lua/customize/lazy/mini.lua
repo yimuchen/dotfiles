c52921786a3522c5a4a42bbb4b164a7c8ee771a4
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-08-28 14:31:08 +0200

    Now using mini-starter to handle start up asynchronously

--- nvim/lua/customize/lazy/mini.lua
+++ nvim/lua/customize/lazy/mini.lua
@@ -1,27 +1,30 @@
-  return {
-    'echasnovski/mini.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' }, -- For scope awareness
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      local spec_treesitter = require('mini.ai').gen_spec.treesitter
-      require('mini.ai').setup {
-        custom_textobjects = {
-          ['F'] = spec_treesitter { a = '@function.outer', i = '@function.inner' },
-            ['C'] = spec_treesitter { a = '@code_cell.outer', i = '@code_cell.inner' }
-        }
-      }
+return {
+  'echasnovski/mini.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' }, -- For scope awareness
+  config = function()
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [']quote
+    --  - ci'  - [C]hange [I]nside [']quote
+    local spec_treesitter = require('mini.ai').gen_spec.treesitter
+    require('mini.ai').setup {
+      custom_textobjects = {
+        ['F'] = spec_treesitter { a = '@function.outer', i = '@function.inner' },
+        ['C'] = spec_treesitter { a = '@code_cell.outer', i = '@code_cell.inner' },
+      },
+    }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-    end,
-  } -- Collection of various small independent plugins/modules
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require('mini.surround').setup()
 
+    -- Custom startup screen
+    -- As this requires custom function, this will be setup in the plugins directory
+    -- require('mini.starter').setup({})
+  end,
+} -- Collection of modules in the minis collection
