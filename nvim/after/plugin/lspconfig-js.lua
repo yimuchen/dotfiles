cc42a9b8abfe4718556b8472afb6c1aacf23b71f
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-09-02 16:59:10 +0200

    Using JQ by default to format JSON files

--- nvim/after/plugin/lspconfig-js.lua
+++ nvim/after/plugin/lspconfig-js.lua
@@ -5,7 +5,7 @@ require('lspconfig').cssls.setup {}
 -- Formatting set
 local conform = require 'conform'
 
--- Setting the prettier to detect the pretteir install the node environment
+-- Setting the prettier to detect the prettier install the node environment
 conform.formatters.npx_prettier = {
   command = 'npx',
   args = { 'prettier', '--stdin-filepath', '$FILENAME' },
@@ -14,3 +14,6 @@ conform.formatters_by_ft.html = { 'npx_prettier' }
 conform.formatters_by_ft.css = { 'npx_prettier' }
 conform.formatters_by_ft.javascript = { 'npx_prettier' }
 conform.formatters_by_ft.svelte = { 'npx_prettier' }
+
+-- Setting a separate parser for JSON using JQ (which is always available for nix-based installs)
+conform.formatters_by_ft.json = { 'jq' }
