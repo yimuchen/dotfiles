29f96a81cabb6d778475b8c5b0ac572f8f2ee113
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2024-11-26 10:55:49 +0100

    Additional binding required for cmssw configuration

--- zsh/machine.sh
+++ zsh/machine.sh
@@ -1,7 +1,3 @@
-# Loading CMSSW configurations if possible
-if [[ -d "/cvmfs/cms.cern.ch" ]] ; then
-  source $HOME/.config/zsh/cmssw_tools.sh
-fi
 
 if [[ -z "${DOMAIN_NAME}" ]]; then
   export DOMAIN_NAME=$(domainname -d)
@@ -13,11 +9,18 @@ if [[ "$HOST" == "ensc"* ]]; then # Personal machine
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232 # BLACK
   typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=235 # DARK GRAY
   typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=3   # Orange
+elif [[ "$HOST" == "hepcms"* ]]; then 
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7   # White
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232 # BLACK
+  typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=235 # DARK GRAY
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=3   # Orange
+  export CMSSW_APPTAINER_BINDPATH="--bind /data --bind /data2"
 elif [[ "$HOST" == "cmslpc"* ]]; then
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=3 # ORANGE
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_BACKGROUND=3 # ORANGE
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_FOREGROUND=232
+  export CMSSW_APPTAINER_BINDPATH="--bind $(readlink $HOME) --bind $(readlink -f ${HOME}/nobackup/) --bind /uscms_data --bind /cvmfs"
 elif [[ "$HOST" == "lxplus"* && "$DOMAIN_NAME" == "cern.ch" ]]; then
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=19 # DEEP BLUE
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=7
@@ -28,10 +31,17 @@ elif [[ "$DOMAIN_NAME" == "etp.kit.edu" ]]; then  # KIT RELATED MACHINES
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=7
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_BACKGROUND=22 # DEEP GREEN
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_FOREGROUND=7
+  export CMSSW_APPTAINER_BINDPATH="--bind /ceph --bind /work"
 else
   ## HOSTNAME NOT LISTED!!
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=1          # RED
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232        # BLACK
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_BACKGROUND=1   # RED
   typeset -g POWERLEVEL9K_CONTEXT_REMOTE_FOREGROUND=232 # BLACK
+fi
+
+# Loading CMSSW configurations if possible (some configurations requires
+# knowing the cluster configuration)
+if [[ -d "/cvmfs/cms.cern.ch" ]] ; then
+  source $HOME/.config/zsh/cmssw_tools.sh
 fi
