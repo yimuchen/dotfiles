5ec0333ee07233bd41da168c2c2c203fd8d06b6c
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-01-11 20:33:22 +0100

    Splitting plasma configurations into sensible chunks

---
+++ nix-config/modules/graphical/plasma-workspace/shortcuts.nix
@@ -0,0 +1,18 @@
+{ }: {
+  # Windows core shortcuts
+  kwin = {
+    "Show Desktop" = "Meta+D";
+    "Window Close" = "Alt+F4";
+    "Walk Through Windows"="Meta+Tab,Alt+Tab,Walk Through Windows";
+    "Window Maximize" = "Meta+PgUp";
+    "Window Minimize" = "Meta+PgDown";
+    "Window Quick Tile Bottom" =
+      "Meta+Down,Meta+Down,Quick Tile Window to the Bottom";
+    "Window Quick Tile Left" =
+      "Meta+Left,Meta+Left,Quick Tile Window to the Left";
+    "Window Quick Tile Right" =
+      "Meta+Right,Meta+Right,Quick Tile Window to the Right";
+    "Window Quick Tile Top" = "Meta+Up,Meta+Up,Quick Tile Window to the Top";
+    "Expose" = "Meta+Q,Ctrl+F9,Toggle Present Windows (Current desktop)";
+  };
+}
---
+++ nix-config/modules/graphical/plasma-workspace/workspace-theme.nix
@@ -0,0 +1,10 @@
+{ }: {
+  theme = "Arc-Dark";
+  colorScheme = "Breeze Dark";
+  lookAndFeel = "org.kde.breezedark.desktop";
+  cursor = {
+    theme = "Adwaita";
+    size = 24;
+  };
+  iconTheme = "Papirus-Dark";
+}
