224e513a5cffd2a61cc20b9a9273c8c486c2e594
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-01-21 16:21:59 +0100

    Moving imgbrowser stuff to be more handled by pure zsh rather than nix

--- nix-config/modules/termgraphics.nix
+++ nix-config/modules/termgraphics.nix
@@ -1,25 +1,5 @@
-{ pkgs, config, ... }:
-let
-  _pdf2png = pkgs.writeShellApplication {
-    name = "_pdf2png";
-    text = # bash
-      ''
-        ${pkgs.ghostscript}/bin/gs -sDEVICE=pngalpha -o %stdout -r144 -dBATCH -dNOPAUSE -dQUIET "$1"
-      '';
-  };
-
-  _icat = pkgs.writeShellApplication {
-    name = "_icat";
-    text = # bash
-      ''
-        ${pkgs.kitty}/bin/kitten icat "$@"
-      '';
-  };
-
-in {
+{ pkgs, config, ... }: {
   home.packages = [
-    _icat # Making a thin alias of kitty terminals icat
-    _pdf2png # Making default method for casting PDF to png images
     pkgs.pdftk # For PDF image manipulation
     pkgs.file # To get information about the file
   ];
@@ -31,11 +11,12 @@ in {
   programs.zsh = {
     envExtra = # bash
       ''
-        export PATH=$PATH:$HOME/.config/zsh/termgraphics
+        # Exposing non-standard binaries to the entire zsh session (because
+        # scripts needs to be able to also load the function defined here)
+        export NIX_EXEC_GS=${pkgs.ghostscript}/bin/gs
+        export NIX_EXEC_KITTEN=${pkgs.kitty}/bin/kitten
+        # Adding the helper functions exposing the fzf item
+        source $HOME/.config/zsh/termgraphics.sh
       '';
-    shellAliases = {
-      "imgbrowse" =
-        "find . -name '*.pdf' -o -name '*.png' -o -name '*.jpg' | fzf --preview 'fzf-img-preview {}' --preview-window=right,65%";
-    };
   };
 }
