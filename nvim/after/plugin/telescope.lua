8f7dea7a85f80065cf5064b4c744b03ef6e73839
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-04-25 15:02:08 +0200

    Attemp to have actions follow the cursor

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,15 +1,6 @@
 local ts_builtin = require 'telescope.builtin'
 local ts_themes = require 'telescope.themes'
 
-local spell_suggest = function()
-  ts_builtin.spell_suggest(ts_themes.get_cursor {
-    prompt_title = 'spelling suggestions',
-    layout_config = {
-      height = 0.25,
-      width = 0.25,
-    },
-  })
-end
 
 local lsp_action = function()
   vim.lsp.buf.code_action(ts_themes.get_cursor {
@@ -24,7 +15,7 @@ end
 local wk = require 'which-key'
 wk.add {
   -- Actions to perform on the specific item variable
-  { '<leader>a', group = '[A]ction' },
-  { '<leader>as', spell_suggest, desc = '[A]ction [S]pell sugggestion', icon = '' },
-  { '<leader>af', lsp_action, desc = '[A]ction LSP [F]ix' },
+  { '<leader>a',  group = '[A]ction' },
+  { '<leader>af', lsp_action,         desc = '[A]ction LSP [F]ix' },
+  { '<leader>ar', vim.lsp.buf.rename, desc = '[A]ction [R]ename' },
 }
