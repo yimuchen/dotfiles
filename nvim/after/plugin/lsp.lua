4ae304a1115ef729368c6b4284e2016f7ffe0cfe
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-04-25 13:04:25 +0200

    Fixing typos

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,6 +1,5 @@
--- Common lsp to load for multiple languages
+-- Common LSP to load for multiple languages
 vim.lsp.enable("harper_ls")
-
 
 -- Additional settings that will be enabled if LSP is available
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -9,10 +8,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local ts_builtin = require 'telescope.builtin'
     local wk = require 'which-key'
 
+    -- Functions for diagnostic jumping
     local diag_jump = function(dir)
       return function()
         vim.diagnostic.jump { count = dir, float = true }
       end
+    end
+
+    -- Slightly nicer hover
+    local hover_override = function()
+      vim.lsp.buf.hover({ border = "rounded" })
     end
 
     -- Navigation based on LSP related functions. For multiple items, we will be
@@ -29,13 +34,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
       { '<leader>ssd', ts_builtin.lsp_document_symbols,          desc = '[S]search [S]ymbols in [D]ocument' },
       { '<leader>ssw', ts_builtin.lsp_dynamic_workspace_symbols, desc = '[S]earch [S]ymbols in [W]orkspace' },
       { '<leader>sd',  ts_builtin.diagnostics,                   desc = '[S]earch [D]iagnostics' },
-      -- Opening preview buffers - Use ctrl key as modifier!
-      { '<C-h>',       vim.lsp.buf.hover,                        desc = '[H]over Documentation',            mode = 'ni' },
+      -- Opening preview buffers - Use ctrl key as modifier
+      { '<C-h>',       hover_override,                           desc = '[H]over Documentation',            mode = 'ni' },
       { '<C-e>',       vim.diagnostic.open_float,                desc = 'Show diagnostic [E]rror messages', mode = 'ni' },
-      -- Actions that will directly modify the text buffer
-      { '<leader>a',   group = '[A]lter' },
-      { '<leader>ar',  vim.lsp.buf.rename,                       desc = '[A]ction [R]ename' },
-      -- { '<leader>af', vim.lsp.buf.code_action, desc = '[A]ction LSP [F]ix' },
     }
 
     -- The following two auto commands are used to highlight references
