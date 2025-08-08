f6f6cf46a4c1a0b893a31d0935ef375aad4a8090
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-08-08 14:27:16 +0200

    Adding tab style autocomplete

--- nvim/lua/customize/lazy/llm.lua
+++ nvim/lua/customize/lazy/llm.lua
@@ -4,39 +4,77 @@ if vim.uv.os_gethostname() == "enscFramework" then
 end
 
 return {
-  "yetone/avante.nvim",
-  build = function()
+  {
+    "yetone/avante.nvim",
+    build = function()
       return "make BUILD_FROM_SOURCE=true"
-  end,
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  ---@module 'avante'
-  ---@type avante.Config
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "ollama",
-    providers = {
-      ollama = {
-        endpoint = endpoint,
-        model = "deepseek-r1:14b",
+    end,
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "ollama",
+      providers = {
+        ollama = {
+          endpoint = endpoint,
+          model = "deepseek-r1:14b",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
       },
     },
   },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-    "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+  {
+    'milanglacier/minuet-ai.nvim',
+    config = function()
+      require('minuet').setup {
+        provider = 'openai_fim_compatible',
+        n_completions = 1, -- recommended for local model for resource saving
+        context_window = 1024,
+        provider_options = {
+          openai_fim_compatible = {
+            api_key = 'TERM',
+            name = 'Ollama',
+            end_point = endpoint .. '/v1/completions',
+            model = 'qwen2.5-coder:7b',
+            optional = { max_tokens = 1024, top_p = 0.9 },
+          },
+        },
+        virtualtext = {
+          auto_trigger_ft = {},
+          keymap = {
+            -- accept whole completion
+            accept = '<A-A>',
+            -- accept one line
+            accept_line = '<A-a>',
+            -- accept n lines (prompts for number)
+            -- e.g. "A-z 2 CR" will accept 2 lines
+            accept_n_lines = '<A-z>',
+            -- Cycle to prev completion item, or manually invoke completion
+            prev = '<A-[>',
+            -- Cycle to next completion item, or manually invoke completion
+            next = '<A-]>',
+            dismiss = '<A-e>',
+          },
+        },
+      }
+    end,
+  }
 }
