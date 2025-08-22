567ff46c76fd1597ea751edc48703279268d5fc0
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2025-08-22 09:11:37 +0200

    Making rose-pine transparent

--- nvim/lua/customize/lazy/theme.lua
+++ nvim/lua/customize/lazy/theme.lua
@@ -6,6 +6,18 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
+      require("rose-pine").setup({
+        enable = {
+          terminal = true,
+          legacy_highlights = true,
+          migrations = true,
+        },
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true,
+        }
+      })
       vim.cmd("colorscheme rose-pine-moon")
     end
   },
