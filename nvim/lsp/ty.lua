31c246ae473dbb7c9ba2ea63c8fc0d49bdd90a24
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2026-03-15 08:48:39 +0100

    Adding ty to list of maintained LSP configurations

---
+++ nvim/lsp/ty.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = { vim.g.get_prefixed_exec('ty'), 'server' },
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+  }
+}
