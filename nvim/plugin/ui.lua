e312c2406c2dacffbb38fc737d73be2204c5386a
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2026-05-06 13:12:25 +0200

    Changing to using nvim.ui2 instead of noice

--- nvim/plugin/ui.lua
+++ nvim/plugin/ui.lua
@@ -6,14 +6,22 @@ vim.pack.add({
   'https://github.com/Bekaboo/dropbar.nvim',
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/nvim-tree/nvim-web-devicons',
-  'https://github.com/folke/noice.nvim',
-  'https://github.com/MunifTanjim/nui.nvim',
-  'https://github.com/rcarriga/nvim-notify',
 })
+-- Using new neovim UI2 framework for handling messages
+require("vim._core.ui2").enable({
+  enabled = true,
+  msg = { -- Send vim.api.nvim_echo items to a transient items at the corner
+    targets = 'msg',
+    msg = { height = 0.5, timeout = 5000 },
+  },
+})
+-- Having items be a rounded item
+vim.opt.winborder = "rounded"
 
 -- Setting up the color scheme
 require("nord").setup({ transparent = true })
 vim.cmd.colorscheme("nord")
+
 
 -- Setting up the bottom line
 require('lualine').setup {
@@ -32,29 +40,32 @@ require('lualine').setup {
   },
 }
 
--- Setting up the UI via noice/notify
-require('notify').setup({ background_colour = '#000000', stages = 'static' })
-require('noice').setup({
-  views = {
-    hover = { border = { style = "rounded", } }
-  },
-  lsp = {
-    documentation = {
-      view = "vsplit",
-      opts = {
-        enter = false,
-        position = "right",
-        size = "40%"
-      }
-    },
-  }
+-- LSP progress bar, taken from
+-- https://www.reddit.com/r/neovim/comments/1rcvliq/ghostty_lsp_progress_bar/
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local value = ev.data.params.value or {}
+    local msg = value.message or "done"
+    -- rust analyszer in particular has really long LSP messages so truncate them
+    if #msg > 40 then
+      msg = msg:sub(1, 37) .. "..."
+    end
+    vim.api.nvim_echo({ { msg } }, false, {
+      id = "lsp",
+      kind = "progress",
+      title = value.title,
+      source = 'lsp-progress',
+      status = value.kind ~= "end" and "running" or "success",
+      percent = value.percentage,
+    })
+  end,
 })
 
 -- Setting up in indent guilds
 local highlight = { 'indent_guide_nonhl' }
 local hooks = require('ibl.hooks')
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, 'indent_guide_nonhl', { fg = '#333333' })
+  vim.api.nvim_set_hl(0, 'indent_guide_nonhl', { fg = '#666666' })
 end)
 require('ibl').setup {
   enabled = true,
